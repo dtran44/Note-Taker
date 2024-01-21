26bd9c7d585405714c96cf26152f84093c1ad8b5
Diana Tran <diana.tran44@gmail.com>
2024-01-21 23:34:54 +1100

    updated js

---
+++ Develop/routes/notes.js
@@ -0,0 +1,32 @@
+const notes = require('express').Router();
+const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
+
+
+// GET Route for retrieving all the notes
+notes.get('/', (req, res) => {
+  console.info(`${req.method} request received for notes`);
+  readFromFile('./db/notes.json').then((data) => res.json(JSON.parse(data)));
+});
+
+// POST Route for a new notes
+notes.post('/', (req, res) => {
+  console.info(`${req.method} request received to add a note`);
+  console.log(req.body);
+
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNote = {
+      title,
+      text,
+    
+    };
+
+    readAndAppend(newNote, './db/notes.json');
+    res.json(`Note added successfully`);
+  } else {
+    res.error('Error in adding note');
+  }
+});
+
+module.exports = notes;
---
+++ Develop/server.js
@@ -0,0 +1,38 @@
+// Import Express.js
+const express = require('express');
+
+// Import built-in Node.js package 'path' to resolve path of files that are located on the server
+const path = require('path');
+
+// Import notes.js from routes folder
+const api = require('./routes/notes.js');
+
+// Initialize an instance of Express.js
+const app = express();
+
+// Specify on which port the Express.js server will run
+const PORT = 3001;
+
+
+// Middleware for parsing JSON and urlencoded form data
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api/notes', api);
+
+app.use(express.static('public'));
+
+// GET Route for homepage
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/index.html'))
+);
+
+// GET Route for notes page
+app.get('/feedback', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/notes.html'))
+);
+
+
+// listen() method is responsible for listening for incoming connections on the specified port 
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
